aabdc7854282e98cd6086f102959abc2085c9304
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-09-17 20:35:07 +0500

    Folders made

---
+++ 01.Local-Server/index2.js
@@ -0,0 +1,28 @@
+console.log(process.platform)
+console.log(process.env.USER)
+
+// const {readFile} = require('fs').promises;
+// const file = await =()=>{
+// 	console.log('shehzad');
+// }
+
+
+//.get is not to modify data reather just to get data
+// .gets 1st argument is the path user will navigate to
+app.get('/', async (request, response) => {
+
+	// if(err) response.status(500).send("sorry, out of Order!");
+
+	response.send(await console.log(`Shehzad`))
+	// console.log(`Shehzad`)
+
+})
+
+
+
+
+app.listen(process.env.PORT || 3000, () => console.log(`Shehzad app is avaliable on local host 3000`))
+
+
+// ? process.env.NODE_ENV
+// use promises instead of callbacks to avoid callbacks hell
