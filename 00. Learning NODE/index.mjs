ef429c71ef6fff10b517eac9c00bb76e0d460c22
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-09 16:26:45 +0500

    Push

--- 00. Learning NODE/index.mjs
+++ 00. Learning NODE/index.mjs
@@ -1,3 +1,19 @@
 console.log(process.platform)
 console.log(process.env.USER)
 
+
+
+
+
+
+
+//to input and output name in terminal like c++
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+  });
+  
+  readline.question('Who are you?', name => {
+	console.log(`Hey there ${name}!`);
+	readline.close();
+  });
