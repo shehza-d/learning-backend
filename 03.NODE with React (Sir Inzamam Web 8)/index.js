900ea4533b422adb899695b348fb4ac339ff67c6
Shehzad <shehzaddiqbal@gmail.com>
2022-11-21 23:14:13 +0500

    server with TypeScipt

--- 03.NODE with React (Sir Inzamam Web 8)/index.js
+++ 03.NODE with React (Sir Inzamam Web 8)/index.js
@@ -1,3 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// const express = require('express')
+const app = (0, express_1.default)();
+const port = 3000;
 console.log("Hello World! Ultra pro max");
+app.get("/", (req, res) => {
+    res.send(`"Hello World!", ${req.ip}`);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+// npm i --save-dev @types/express
--- 03.NODE with React (Sir Inzamam Web 8)/index.ts
+++ 03.NODE with React (Sir Inzamam Web 8)/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World! Ultra pro max") 
+import express from "express";
+// const express = require('express')
+const app = express();
+const port = 3000;
+console.log("Hello World! Ultra pro max");
+
+app.get("/", (req, res) => {
+  res.send(`"Hello World!", ${req.ip}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+// npm i --save-dev @types/express
