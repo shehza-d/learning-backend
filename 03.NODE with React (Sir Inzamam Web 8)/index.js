f48000a4e16192b0d9c9b041e262e60454b1bf51
Shehzad” <shehzad.dev@pm.me>
2022-11-23 22:13:51 +0500

    New Notes

--- 03.NODE with React (Sir Inzamam Web 8)/index.js
+++ 03.NODE with React (Sir Inzamam Web 8)/index.js
@@ -1,17 +1,14 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+exports.__esModule = true;
+var express_1 = require("express");
 // const express = require('express')
-const app = (0, express_1.default)();
-const port = 3000;
+var app = (0, express_1["default"])();
+var port = 3000;
 console.log("Hello World! Ultra pro max");
-app.get("/", (req, res) => {
-    res.send(`"Hello World!", ${req.ip}`);
+app.get("/", function (req, res) {
+    res.send("\"Hello World!\", ".concat(req.ip));
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(port, function () {
+    console.log("Example app listening on port ".concat(port));
 });
 // npm i --save-dev @types/express
