c6ff06e5a09ee5aa0895b19efb2563dc3408d71c
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-08-26 21:52:43 +0500

    scripting

---
+++ AWS/assignments/script.mjs
@@ -0,0 +1,22 @@
+import { exec } from "child_process";
+import fs from "fs";
+
+exec("aws iam list-users", (error, stdout) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  fs.writeFileSync("aws_iam_users.txt", stdout);
+
+  console.log(`Done!`);
+});
+
+exec("aws iam list-groups", (error, stdout) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  fs.writeFileSync("aws_iam_users_groups.txt", stdout);
+
+  console.log(`Done!`);
+});
